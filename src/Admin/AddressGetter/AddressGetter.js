5c8179ed7ffb8465260e01a675424be3bbd7ca92
Rusanov <rusanov@htsl.ru>
2018-10-07 19:35:24 +0300

    some markup

--- src/Admin/AddressGetter/AddressGetter.js
+++ src/Admin/AddressGetter/AddressGetter.js
@@ -1,14 +1,16 @@
 import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import style from './style.less';
-// require('./style.less')
 
 class AddressGetter extends React.Component {
   render() {
-    console.log(style);
     return <Fragment>
       <div className={style.topBlock}></div>
-      <div className={style.mainBlock}>Main content</div>
+      <div className={style.mainBlock}>
+        <div className={style.greeting}>Home address</div>
+        <div>Please enter the director’s home address for the last 3 years.</div>
+        <div className={style.splitter}></div>
+      </div>
     </Fragment>;
   }
 }
